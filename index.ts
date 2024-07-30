2dfd3c816bdc9fe9b90707d1f1c660663b6ab078
Isahir Zavala Miranda <isahirzavala@hotmail.com>
2024-07-30 17:08:20 -0500

    removed object generation to use the flash gemini instead(better analysis)

--- index.ts
+++ index.ts
@@ -4,8 +4,6 @@ import { google } from "@ai-sdk/google";
 import { openai } from '@ai-sdk/openai'
 import { generateObject, generateText, ImagePart, TextPart } from "ai";
 import dotenv from 'dotenv'
-import { z } from "zod";
-
 dotenv.config()
 const app = express();
 
@@ -16,32 +14,23 @@ app.get("/", (req: Request, res: Response) => {
   res.status(200).send("Hello there!");
 });
 
-// Definimos el esquema Zod para la respuesta
-const ProfileAnalysisSchema = z.object({
-  description: z.string(),
-  mbti: z.string()
-});
-
-// Creamos el tipo TypeScript basado en el esquema Zod
-type ProfileAnalysis = z.infer<typeof ProfileAnalysisSchema>;
 
 app.get("/getposts/:username", async (req, res) => {
   try {
     let username = req.params.username;
     const posts = await fetchInstagramPosts(username);
 
-    const model = google('models/gemini-1.5-pro-latest');
+    const model = google('models/gemini-1.5-flash-latest');
 
     const content: (TextPart | ImagePart)[] = [
       { type: 'text', text: 'analizame este perfil' } as TextPart,
       ...posts.map(url => ({ type: 'image', image: url } as ImagePart)),
     ];
 
-    const analysis  = await generateObject<ProfileAnalysis>({
+    const analysis  = await generateText({
       model: model,
-      schema: ProfileAnalysisSchema,
       system: `Eres un analizador de perfiles de instagram, recibiras varias fotografias y daras una descripion profunda de la persona en base a estas, 
-      retorna una descripcion de no mas de 100 palabras y minima de 80 y si es posible trata de dar el mbti del usuario
+      retorna una descripcion de no mas de 120 palabras y minima de 80 y si es posible trata de dar el mbti del usuario
 
       Basate solo en lo visto en las fotos, intenta dar una descripcion lo mas precisa posible, para que parezca quee es algo mas personal
 
@@ -56,7 +45,7 @@ app.get("/getposts/:username", async (req, res) => {
       ],
     });
 
-    res.send(analysis.object);
+    res.send(analysis.text);
 
 
   }
